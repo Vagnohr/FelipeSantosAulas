4a45d653350a9274ae2a55481efc49d8f7a2fb54
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-04 21:18:47 -0300

    Adicionado mais informações
    adicionado mais informações sobre o passado e coisas que gosta

--- exe01.py
+++ exe01.py
@@ -1,3 +1,4 @@
 #Felipe Bortoluzzi dos Santos
 #16 anos
-#Gosto de Jogar jogos e ler livros
+#Gosto de Jogar jogos, ler livros e esportes, como volei e futsal
+#Diagnosticado com autismo aos 10 anos de idade
