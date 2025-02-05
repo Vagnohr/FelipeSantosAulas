700afada892fc26dd25680c8884e3b6278a8ef7b
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-04 21:38:22 -0300

    Update exe01.py

--- exe01.py
+++ exe01.py
@@ -2,4 +2,3 @@
 #16 anos
 #Gosto de Jogar jogos, ler livros e esportes, como volei e futsal
 #Diagnosticado com autismo aos 10 anos de idade
-#Eu amo a aula de zumba mas amo mesmo a de programassão de aplicativo
