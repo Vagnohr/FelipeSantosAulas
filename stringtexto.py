edd8db04e727db650630d791c76b0e6e15da27a8
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-10 20:42:02 -0300

    Update stringtexto.py

--- stringtexto.py
+++ stringtexto.py
@@ -5,3 +5,8 @@ print(email[0])
 
 print("\nPegando da posição ZERO até a ONZE do email atribuido:")
 print(email[0:12])
+
+email = "felipebtst1@gmail.com"
+
+print("\nUsando metodo LEN para saber o tamanho da variavel email:")
+print(len(email))
