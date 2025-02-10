e1cac70ef000a3f2d49c70fb24370172459de738
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-10 20:38:00 -0300

    Update stringtexto.py

--- stringtexto.py
+++ stringtexto.py
@@ -3,5 +3,5 @@ email = "felipebtst1@gmail.com"
 print("\nPEgando posição ZERO no email atribuindo:")
 print(email[0])
 
-print("znPegando da posição ZERO até a ONZE do email atribuido:")
-print(email[0:11])
+print("\nPegando da posição ZERO até a ONZE do email atribuido:")
+print(email[0:12])
