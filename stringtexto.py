7d83031db46a37ba38f4cd2fbe2b7fbc79d1f0ac
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-10 19:59:36 -0300

    Create stringtexto.py

---
+++ stringtexto.py
@@ -0,0 +1,7 @@
+email = "Sergioluiz@gmail.com"
+
+print("\nPEgando posição ZERO no email atribuindo:")
+print(email[0])
+
+print("znPegando da posição ZERO até a ONZE do email atribuido:")
+print(email[0:11])
