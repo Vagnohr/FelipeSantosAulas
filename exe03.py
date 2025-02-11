b823d0f6b3e20be0c128f1bdf409daeebadcf78b
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-10 21:17:21 -0300

    up

--- exe03.py
+++
@@ -1,5 +0,0 @@
-#Faça um programa que receba um salario de R$1500 e tenha um aumento de 5% do salario e ao final exiba o salario com o aumento
-x = 1500
-y = (x / 100) * 5
-z = y + x
-print (z)
--- formatstring2.py
+++ formatstring2.py
@@ -8,3 +8,17 @@ faturamento = 2000
 custo = 500
 lucro = faturamento - custo
 print("\nO faturamento da loja foi de : {0} O custo foi de : {1} O lucro foi de : {2}".format(faturamento,custo,lucro))
+
+
+
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+print("\nO faturamento da loja foi de : %s" % faturamento)
+
+
+
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+print("\nO faturamento da loja foi de : %d" % faturamento)
