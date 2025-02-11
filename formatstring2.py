bb3942a2ed4fc277d6c3d1c1d14f557f6eb9e50c
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-10 21:05:43 -0300

    Update formatstring2.py

--- formatstring2.py
+++ formatstring2.py
@@ -1,4 +1,4 @@
 faturamento = 2000
 custo = 500
 lucro = faturamento - custo
-print("O faturamento da loja foi de : {}".format(faturamento) + " O custo foi de : {}".format(custo) + " O lucro foi de : {}".format(lucro))
+print("O faturamento da loja foi de : {} O custo foi de : {} O lucro foi de : {}".format(faturamento,custo,lucro))
