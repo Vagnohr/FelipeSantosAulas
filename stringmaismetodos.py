b4b519b3bc7dbeea3310290b23d0979c386e4f5f
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-10 21:32:42 -0300

    Update stringmaismetodos.py

--- stringmaismetodos.py
+++ stringmaismetodos.py
@@ -1,3 +1,13 @@
 #transforma apenas a primeira letra de uma string em maiuscula
 nome = "felipe"
 print(nome.capitalize(),"\n")
+
+
+#transforma todas as letras em minuscula
+nome = "FELIPE"
+print(nome.casefold(), "\n")
+
+
+#conta o numero de vezes que um caractere specifico aparece na string
+nome = "felipebtst1@gmail.com"
+print(nome.count("e"), "\n")
