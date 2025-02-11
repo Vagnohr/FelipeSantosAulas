ce0b4cbd75784e3d381570b8db2bcd14a105ed7f
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-10 21:39:17 -0300

    Update stringmaismetodos.py

--- stringmaismetodos.py
+++ stringmaismetodos.py
@@ -11,3 +11,13 @@ print(nome.casefold(), "\n")
 #conta o numero de vezes que um caractere specifico aparece na string
 nome = "felipebtst1@gmail.com"
 print(nome.count("e"), "\n")
+
+
+#retorna true ou false para um teste se a string termina com uma string specifica
+nome = "felipebtst1@gmail.com"
+print(nome.endswith("gmail.com"), "\n")
+
+
+#encontra a posicção do termo procurado. lembre-se começa do zero
+nome = "felipebtst1@gmail.com"
+print(nome.find("@"), "\n")
