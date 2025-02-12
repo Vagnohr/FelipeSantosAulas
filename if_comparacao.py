e741bfecfae76934a2a5098851a94df5c1ee67d7
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-11 21:03:13 -0300

    Update if_comparacao.py

--- if_comparacao.py
+++ if_comparacao.py
@@ -3,3 +3,7 @@ if "@" in "felipebtst1@gmail.com":
     print("O email é válido")
 else:
     print("O email não é válido")
+if not "#" in "felipebtst1@gmail.com":
+    print("NÃO TEM O #")
+else:
+    print("TEM O #")
