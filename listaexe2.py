fb99bf76ba3d8cbdd3cc90f93bea243403923725
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-13 20:44:39 -0300

    Create listaexe2.py

---
+++ listaexe2.py
@@ -0,0 +1,5 @@
+produtos=["tv","celular","tablet","mouse","teclado","geladeira","forno"]
+estoque=[100,150,100,120,70,180,80]
+produto=input("Insira o nome do produto em letra minuscula ")
+i=produtos.index(produto)
+print(i)
