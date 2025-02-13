74e06e2f89ad23cebfe51f82629f0578332ee56e
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-13 20:55:16 -0300

    Update listaexe2.py

--- listaexe2.py
+++ listaexe2.py
@@ -1,5 +1,9 @@
 produtos=["tv","celular","tablet","mouse","teclado","geladeira","forno"]
 estoque=[100,150,100,120,70,180,80]
-produto=input("Insira o nome do produto em letra minuscula ")
-i=produtos.index(produto)
-print(i)
+produto=input("Insira o nome do produto em letra minuscula: ")
+if produto not in produtos:
+    print("Produto não registrado")
+else:
+    i=produtos.index(produto)
+    print(str(produtos[i]))
+    print(str(estoque[i]))
