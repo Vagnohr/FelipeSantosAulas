38ae98bc1fb4727f6c189b49ecdd7e7c0e37e072
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-13 19:54:53 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,12 @@
+#a estrutura da lista é essa:
+listab = ["item 1", "item 2", "item 3"]
+listaa = [] #lista vazia
+lista1 = [1,2,3,4,5]
+lista2 = [6,7,8,9,10]
+lista3 = [11,12,13,14,15]
+todas_listas = [lista1,lista2,lista3]
+print(todas_listas)
+listac = [
+    "item1",
+    "item2",
+    "item3"]
