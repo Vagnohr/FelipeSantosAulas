a99c1810f31a7465719905b4c438acc11f37608e
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-13 19:34:01 -0300

    Update ifcontraintuitiva.py

--- ifcontraintuitiva.py
+++ ifcontraintuitiva.py
@@ -1,4 +1,7 @@
 faturamento = input("Qual foi o faturamento da loja nesse mês? ")
 custo = input("Qual foi o custo da loja nesse mês? ")
-lucro = int(faturamento) - int(custo)
-print("O lucro da loja foi de {} R$".format(lucro))
+if faturamento == "" or custo == "":
+    print("ERRO")
+else:
+    lucro = int(faturamento) - int(custo)
+    print("O lucro da loja foi de {} R$".format(lucro))
