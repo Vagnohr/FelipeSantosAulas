f98d9f15b273fc6d74e5f0d50867267e6f98b6f0
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-13 21:58:01 -0300

    Create juntarlista3.py

---
+++ juntarlista3.py
@@ -0,0 +1,8 @@
+produtos=["apple","tv","mac","iphone x","IPad","apple watch","mach book"," airpods"]
+novos_produtos=["chromecast","windows phone"]
+print("Usando + :")
+produtos_compilados=produtos+novos_produtos
+print(produtos_compilados)
+print("Usando Append :")
+produtos.append(novos_produtos)
+print(produtos)
