24c15a0af0b155fe62db8aaaa425814a2b2082b8
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-13 21:53:10 -0300

    Create juntarlista2.py

---
+++ juntarlista2.py
@@ -0,0 +1,4 @@
+produtos=["apple","tv","mac","iphone x","IPad","apple watch","mach book"," airpods"]
+novos_produtos=["chromecast","windows phone"]
+produtos_compilado=produtos+novos_produtos
+print(produtos_compilado)
