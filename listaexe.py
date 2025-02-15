e1eb72e673dad39c1ae01f1332e23df91b62167f
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 21:35:51 -0300

    Update listaexe.py

--- listaexe.py
+++ listaexe.py
@@ -6,4 +6,4 @@ print("As vendas de {} foram de {}".format(produtos[1],vendas[1]))
 produtos=["tv","celular","mouse","teclado","tablet"]
 i=produtos.index("mouse")
 print("O valor de I é "+str(i))
-print("O produto da posição I é "+str(produtos[1]))
+print("O produto da posição I é "+str(produtos[i]))
