39175be695d4b29f6b28d1318bb1621558aabbbc
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 21:23:10 -0300

    Update listaerro.py

--- listaerro.py
+++ listaerro.py
@@ -5,4 +5,4 @@ if procurar not in produtos:
     procurar = input("Insira qual produtor deseja procurar: ")
 elif procurar in produtos:
     i=produtos.index(procurar)
-    print("produto:"+str(i))
+    print(produtos[i])
