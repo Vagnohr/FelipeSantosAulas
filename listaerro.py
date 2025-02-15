861cf23068583e08c799e94307115b7f77a6fd20
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 21:57:21 -0300

    Update listaerro.py

--- listaerro.py
+++ listaerro.py
@@ -1,10 +1,14 @@
 clientes=["Felipe","Arthur","João","Gabriel"]
 telefone=[1241241,124124124,141231414,124141]
-bairro=[]
+bairro=["Jardim Iririu","Catapimbas","Carambola","Cebola"]
 procurar = input("Insira qual cliente deseja procurar: ")
 if procurar not in clientes:
     print("pessoa não está na lista")
     procurar = input("Insira qual cliente deseja procurar: ")
 elif procurar in clientes:
     i=clientes.index(procurar)
+    x=bairro.index(procurar)
+    y=telefone.index(procurar)
     print(clientes[i])
+    print(telefone[x])
+    print(bairro[y])
