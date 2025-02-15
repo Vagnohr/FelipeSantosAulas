b2ea1166a4f2d93bf1b0416bc21e353b01a7b573
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 21:21:09 -0300

    Create listaerro.py

---
+++ listaerro.py
@@ -0,0 +1,8 @@
+produtos=["tv","celular","tablet","mouse","teclado","geladeira","forno"]
+procurar = input("Insira qual produtor deseja procurar: ")
+if procurar not in produtos:
+    print("Item não está na lista")
+    procurar = input("Insira qual produtor deseja procurar: ")
+elif procurar in produtos:
+    i=produtos.index(procurar)
+    print("produto:{}".format(str(i)))
