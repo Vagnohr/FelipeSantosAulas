3bb848caa83f41d70ca243d8840ca4d63906a603
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-17 20:57:51 -0300

    up

---
+++ exeforbreak.py
@@ -0,0 +1,8 @@
+pessoas_presentes=["John Snow","Jesse Pinkman","Aria Stark","Tyrion Lannister"]
+chamada = "Aria Stark"
+for pessoa in pessoas_presentes:
+    if pessoa == chamada:
+        print("{} está presente".format(chamada))
+        break
+    else:
+        print("Só um print para mostrar que o o sor passou por essa pessoa:"+str(pessoa))
