4596a4038dafe9a490e0e8bc91c83c0108b3711a
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-17 20:58:45 -0300

    Update exeforbreak.py

--- exeforbreak.py
+++ exeforbreak.py
@@ -6,3 +6,14 @@ for pessoa in pessoas_presentes:
         break
     else:
         print("Só um print para mostrar que o o sor passou por essa pessoa:"+str(pessoa))
+
+pessoas_presentes=["John Snow","Jesse Pinkman","Aria Stark","Tyrion Lannister"]
+chamada = "Aria Stark"
+for pessoa in pessoas_presentes:
+    if pessoa == chamada:
+        print("{} está presente".format(chamada))
+        break
+    else:
+        print("Só um print para mostrar que o o sor passou por essa pessoa:"+str(pessoa))
+        continue
+    print("Só para mostrar qie ossp não vai ser printado")
