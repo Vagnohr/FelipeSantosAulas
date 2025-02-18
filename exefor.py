e84e61664673ddcfa64a5a78c85997787af2ed3d
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-18 19:10:45 -0300

    Update exefor.py

--- exefor.py
+++ exefor.py
@@ -1,3 +1,3 @@
 produtos=["coca cola","pepsi","guarana","sprite","fanta","Agua da Serra"]
 for item in produtos:
-    print("A produção de: []".format(item))
+    print("A produção de: {}".format(item))
