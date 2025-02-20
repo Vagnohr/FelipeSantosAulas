f8860a75e559c8ef3aa0da1bb56c52e404e3ff8a
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-20 19:19:10 -0300

    Create exedicionario2.py

---
+++ exedicionario2.py
@@ -0,0 +1,5 @@
+mais_vendidos={"tecnologia":"iphone","refrigeração":"ar consul 1200 btu","livros":"o alquimista","eletrodomesticos":"geladeira"}
+vendas_tecnologia={"iphone":15000,"sansung galaxy":12000,"Tv Samsung":10000,"ps5":14300,"tabelt":1720,"ipad":1200}
+livro=mais_vendidos["livros"]
+#print(mais_vendidos["geladeiras"])
+#print(mais_vendidos.get("geladeiras"))
