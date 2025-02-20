567c02d5e7c4c5de2ac76c42b73e5f3f4f8d6bb5
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-20 19:43:09 -0300

    Update exedicionario5.py

--- exedicionario5.py
+++ exedicionario5.py
@@ -1,6 +1,7 @@
 lucro_1tri={"janeiro":100000,"fevereiro":120000,"março":90000}
 lucro_2tri={"abril":88000,"maio":89000,"junho":120000}
 #del lucro_2tri("junho")   
-lucro_jun=lucro_2tri.pop("junho")
+#lucro_jun=lucro_2tri.pop("junho")
+lucro_2tri.clear()
 print(lucro_2tri)
-print(lucro_jun)
+#print(lucro_jun)
