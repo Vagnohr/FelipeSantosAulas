1b6f4c041b46cfee736d36b1949a55ed8769b730
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-20 19:56:31 -0300

    Update exedicionario6.py

--- exedicionario6.py
+++ exedicionario6.py
@@ -1,3 +1,7 @@
 vendas_tecnologia={"iphone":15000,"sansung galaxy":12000,"Tv Samsung":10000,"ps5":14300,"tabelt":1720,"ipad":1200}
-for chave in vendas_tecnologia:
-    print("O produto {} vendeu {} unidades.".format(chave,vendas_tecnologia(chave)))
+#for chave in vendas_tecnologia:
+    #print("O produto {} vendeu {} unidades.".format(chave,vendas_tecnologia(chave)))
+#for item in vendas_tecnologia.items():
+    #print(item)
+for produto, vendas in vendas_tecnologia.items():
+    print("{}: {} de unidades.".format(produto,vendas))
