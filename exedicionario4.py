476f4e92703eb390fcbc9c2ccbd4236ae1c0a29e
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-20 19:31:03 -0300

    Create exedicionario4.py

---
+++ exedicionario4.py
@@ -0,0 +1,9 @@
+vendas_vendedores={}
+vendas_vendedores["Joao"]=100
+print(vendas_vendedores)
+vendas_vendedores.update({"Lira":50,"Nathalie":80})
+print(vendas_vendedores)
+vendas_vendedores["Joao"]=70
+print(vendas_vendedores)
+vendas_vendedores.update({"Lira":67,"Nathalie":12})
+print(vendas_vendedores)
