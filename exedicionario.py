21d847cde6621e02e7b4271157afe31ee524ebcb
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-20 19:11:54 -0300

    Update exedicionario.py

--- exedicionario.py
+++ exedicionario.py
@@ -1,6 +1,6 @@
 mais_vendidos={"tecnologia":"iphone","refrigeração":"ar consul 1200 btu","livros":"o alquimista","eletrodomesticos":"geladeira"}
 vendas_tecnologia={"iphone":1500,"sansung galaxy":12000,"Tv Samsung":10000,"ps5":1400,"tabelt":1720,"ipad":1200}
-livro=mais_vendidos("livros")
+livro=mais_vendidos["livros"]
 print(mais_vendidos.get("livros"))
-print(mais_vendidos("livros"))
+print(mais_vendidos["livros"])
 print("FIn programa")
