5880f3bad3dc69ad705ab0d47a796edc8d0bec0a
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-21 19:36:33 -0300

    Create exetxt2.py

---
+++ exetxt2.py
@@ -0,0 +1,8 @@
+arquivo=open("C:/Users/felipe_santos154/Documents/txt/GOT.txt","w")
+arquivo.write("Escrevendo um arquivo\n")
+arquivo.write("John Snow\n")
+arquivo.write("Arya Stark\n")
+arquivo.write("Sansa Stark\n")
+arquivo.write("Bran Stark\n")
+arquivo.close()
+print("Fim programa")
