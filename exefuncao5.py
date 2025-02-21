bcf03db77c0921bc6cab950f5afe4b5015a06be4
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-21 20:58:10 -0300

    Update exefuncao5.py

--- exefuncao5.py
+++ exefuncao5.py
@@ -3,3 +3,8 @@ def get_data():
     idade_usuario=int(input("QUal a sua idade? "))
     tupla_data=(nome_usuario, idade_usuario)
     return tupla_data
+def mensagem(nome_usuario, idade_usuario):
+    if idade_usuario<=10:
+        print("Oi",nome_usuario)
+    else:
+        print("Olá!",nome_usuario)
