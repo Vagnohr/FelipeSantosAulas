0303e75e9ab302174700a2479af75b6d862006cd
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-21 19:49:52 -0300

    Update exetxt3.py

--- exetxt3.py
+++ exetxt3.py
@@ -1,4 +1,4 @@
-with open("C:/Users/felipe_santos154/Documents/txt/Resumo 3.txt", "v") as arquivo3:
+with open("C:/Users/felipe_santos154/Documents/txt/Resumo 3.txt", "w") as arquivo3:
     arquivo3.write("Escrevendo no arquivo\n")
     arquivo3.write("Capitão Stoltomayer\n")
 print("Fim programa")
