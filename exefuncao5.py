4f5b219e5a4b3186e0756773b51bb7634bbb6686
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-21 21:00:55 -0300

    Update exefuncao5.py

--- exefuncao5.py
+++ exefuncao5.py
@@ -1,6 +1,6 @@
 def get_data():
     nome_usuario=input("Escreva o seu nome: ")
-    idade_usuario=int(input("QUal a sua idade? "))
+    idade_usuario=int(input("Qual a sua idade? "))
     tupla_data=(nome_usuario, idade_usuario)
     return tupla_data
 def mensagem(nome_usuario, idade_usuario):
